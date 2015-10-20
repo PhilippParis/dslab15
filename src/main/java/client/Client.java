3c7ed69d869566cead5a3950665aed41755c6677
Philipp Paris <paris.philipp@gmail.com>
2015-10-20 22:59:31 +0200

    client: connect to server implemented

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -3,15 +3,39 @@ package client;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import cli.Command;
+import cli.Shell;
+import domain.IChannel;
+import domain.TCPChannel;
+import service.ChannelService;
+import service.IChannelService;
 import util.Config;
 
 public class Client implements IClientCli, Runnable {
+	private final static Logger LOGGER = Logger.getLogger(Client.class.getName());
 
 	private String componentName;
 	private Config config;
 	private InputStream userRequestStream;
 	private PrintStream userResponseStream;
+
+	// cli
+	private Shell shell;
+
+	// threading
+	private ExecutorService executorService = Executors.newCachedThreadPool();
+
+	// connection
+	private IChannel serverChannel;
+
+	// services
+	private IChannelService channelService;
 
 	/**
 	 * @param componentName
@@ -30,66 +54,103 @@ public class Client implements IClientCli, Runnable {
 		this.userRequestStream = userRequestStream;
 		this.userResponseStream = userResponseStream;
 
-		// TODO
+		// setup services
+		channelService = new ChannelService(executorService);
+
+		// setup shell
+		shell = new Shell(componentName, userRequestStream, userResponseStream);
+		shell.register(this);
 	}
 
 	@Override
 	public void run() {
+		// start shell thread
+		executorService.execute(shell);
+
+		// connect to server
+		connectToServer(config.getString("chatserver.host"), config.getInt("chatserver.tcp.port"));
+	}
+
+	private void connectToServer(String host, int port) {
+		try {
+			Socket socket = new Socket(host, port);
+			serverChannel = new TCPChannel(socket);
+			channelService.addChannel(serverChannel);
+		} catch (IOException e) {
+			LOGGER.log(Level.INFO, "could not connect to server");
+			userResponseStream.println("could not connect to server");
+			shutdown();
+		}
+	}
+
+	/**
+	 * shuts down the client: logs the user out, releases resources, closes open sockets
+	 */
+	private void shutdown() {
 		// TODO
 	}
 
+	@Command
 	@Override
 	public String login(String username, String password) throws IOException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Command
 	@Override
 	public String logout() throws IOException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Command
 	@Override
 	public String send(String message) throws IOException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Command
 	@Override
 	public String list() throws IOException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Command
 	@Override
 	public String msg(String username, String message) throws IOException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Command
 	@Override
 	public String lookup(String username) throws IOException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Command
 	@Override
 	public String register(String privateAddress) throws IOException {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
+	@Command
 	@Override
 	public String lastMsg() throws IOException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Command
 	@Override
 	public String exit() throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		shutdown();
+		return "exiting";
 	}
 
 	/**
@@ -99,7 +160,7 @@ public class Client implements IClientCli, Runnable {
 	public static void main(String[] args) {
 		Client client = new Client(args[0], new Config("client"), System.in,
 				System.out);
-		// TODO: start the client
+		client.run();
 	}
 
 	// --- Commands needed for Lab 2. Please note that you do not have to
